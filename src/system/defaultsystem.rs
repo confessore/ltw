cf3a242eb9019b323176dbccff1dfc06b45b963f
confessore <confessore@balasolu.com>
2021-12-09 14:35:11 -0500

    ui and text

--- src/system/defaultsystem.rs
+++ src/system/defaultsystem.rs
@@ -5,37 +5,67 @@ pub fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>
 ) {
-    let normal = materials.add(Color::rgb(0.15, 0.15, 0.15).into());
-    let hovered = materials.add(Color::rgb(0.25, 0.25, 0.25).into());
-    let pressed = materials.add(Color::rgb(0.35, 0.75, 0.35).into());
+    // ui camera
     commands.spawn_bundle(UiCameraBundle::default());
-    commands.spawn_bundle(ButtonBundle {
+    // root node
+    commands.spawn_bundle(NodeBundle {
         style: Style {
-            size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-            // center button
-            margin: Rect::all(Val::Auto),
-            // horizontally center child text
-            justify_content: JustifyContent::Center,
-            // vertically center child text
-            align_items: AlignItems::Center,
+            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+            justify_content: JustifyContent::SpaceBetween,
             ..Default::default()
         },
-        material: normal.clone(),
+        material: materials.add(Color::NONE.into()),
         ..Default::default()
     })
     .with_children(|parent| {
-        parent.spawn_bundle(TextBundle {
-            text: Text::with_section(
-                "Button",
-                TextStyle {
-                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                    font_size: 40.0,
-                    color: Color::rgb(0.9, 0.9, 0.9),
-                },
-                Default::default(),
-            ),
+        // left vertical border node
+        parent.spawn_bundle(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(15.0), Val::Percent(100.0)),
+                border: Rect::all(Val::Px(2.0)),
+                ..Default::default()
+            },
+            material: materials.add(Color::rgb(0.65, 0.65, 0.65).into()),
             ..Default::default()
+        })
+        .with_children(|parent| {
+            //left vertical content node
+            parent.spawn_bundle(NodeBundle {
+                style: Style {
+                    size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                    align_items: AlignItems::FlexEnd,
+                    ..Default::default()
+                },
+                material: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
+                ..Default::default()
+            })
+            .with_children(|parent| {
+                parent.spawn_bundle(TextBundle {
+                    style: Style {
+                        align_self: AlignSelf::FlexEnd,
+                        position_type: PositionType::Absolute,
+                        position: Rect {
+                            bottom: Val::Percent(5.0),
+                            right: Val::Percent(100.0),
+                            ..Default::default()
+                        },
+                        margin: Rect::all(Val::Px(5.0)),
+                        ..Default::default()
+                    },
+                    text: Text::with_section(
+                        "Text Example",
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 30.0,
+                            color: Color::WHITE
+                        },
+                        TextAlignment {
+                            horizontal: HorizontalAlign::Center,
+                            vertical: VerticalAlign::Center
+                        }),
+                    ..Default::default()
+                });
+            });
         });
     });
-
 }
