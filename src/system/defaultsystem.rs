6eccb533829d9045f97766769b42bf260755d0e2
confessore <confessore@balasolu.com>
2021-12-09 20:52:12 -0500

    bevy ui

--- src/system/defaultsystem.rs
+++ src/system/defaultsystem.rs
@@ -10,7 +10,10 @@ pub fn setup(
     // root node
     commands.spawn_bundle(NodeBundle {
         style: Style {
-            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+            size: Size::new(Val::Percent(15.0), Val::Percent(100.0)),
+            flex_direction: FlexDirection::Column,
+            flex_wrap: FlexWrap::Wrap,
+            align_content: AlignContent::SpaceBetween,
             justify_content: JustifyContent::SpaceBetween,
             ..Default::default()
         },
@@ -21,7 +24,7 @@ pub fn setup(
         // left vertical border node
         parent.spawn_bundle(NodeBundle {
             style: Style {
-                size: Size::new(Val::Percent(15.0), Val::Percent(100.0)),
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
                 border: Rect::all(Val::Px(2.0)),
                 ..Default::default()
             },
@@ -33,37 +36,46 @@ pub fn setup(
             parent.spawn_bundle(NodeBundle {
                 style: Style {
                     size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                    align_items: AlignItems::FlexEnd,
+                    align_content: AlignContent::SpaceBetween,
+                    justify_content: JustifyContent::SpaceBetween,
                     ..Default::default()
                 },
                 material: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
                 ..Default::default()
             })
             .with_children(|parent| {
-                parent.spawn_bundle(TextBundle {
+                parent.spawn_bundle(NodeBundle {
                     style: Style {
-                        align_self: AlignSelf::FlexEnd,
-                        position_type: PositionType::Absolute,
-                        position: Rect {
-                            bottom: Val::Percent(5.0),
-                            right: Val::Percent(100.0),
-                            ..Default::default()
-                        },
-                        margin: Rect::all(Val::Px(5.0)),
+                        size: Size::new(Val::Percent(100.0), Val::Percent(5.0)),
+                        align_content: AlignContent::SpaceBetween,
+                        justify_content: JustifyContent::SpaceBetween,
                         ..Default::default()
                     },
-                    text: Text::with_section(
-                        "Text Example",
-                        TextStyle {
-                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                            font_size: 30.0,
-                            color: Color::WHITE
-                        },
-                        TextAlignment {
-                            horizontal: HorizontalAlign::Center,
-                            vertical: VerticalAlign::Center
-                        }),
+                    material: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
                     ..Default::default()
+                })
+                .with_children(|parent| {
+                    parent.spawn_bundle(TextBundle {
+                        style: Style {
+                            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                            align_content: AlignContent::SpaceBetween,
+                            justify_content: JustifyContent::SpaceBetween,
+                            margin: Rect::all(Val::Px(5.0)),
+                            ..Default::default()
+                        },
+                        text: Text::with_section(
+                            "Text Example",
+                            TextStyle {
+                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                font_size: 30.0,
+                                color: Color::WHITE
+                            },
+                            TextAlignment {
+                                horizontal: HorizontalAlign::Center,
+                                vertical: VerticalAlign::Center
+                            }),
+                        ..Default::default()
+                    });
                 });
             });
         });
